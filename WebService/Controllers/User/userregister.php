8a6d5450e1272b9195bb41adccf676177b1e32cc
CursedSheep <132466484+MemExplorer@users.noreply.github.com>
2023-06-07 12:01:07 +0800

    Added Cart API

--- WebService/Controllers/User/userregister.php
+++ WebService/Controllers/User/userregister.php
@@ -3,33 +3,36 @@ include_once('../../connection.php');
 include_once('../../Models/response.php');
 include_once('../../utils.php');
 
-$username = $_GET['usrnm'];
-$password = $_GET['pswd'];
-
 try {
-    $stmt = $conn->prepare("SELECT * FROM tbluserlogin WHERE username = ?");
-    $stmt->bind_param("s", $username);
-    $stmt->execute();
+    if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+        $username = $_GET['usrnm'];
+        $password = $_GET['pswd'];
+        $stmt = $conn->prepare("SELECT * FROM tbluserlogin WHERE username = ?");
+        $stmt->bind_param("s", $username);
+        $stmt->execute();
 
-    $result = $stmt->get_result();
+        $result = $stmt->get_result();
 
-    // Check if the user exists
-    if ($result->num_rows > 0) {
-        Response::CreateResponse(false, "Username already exists!");
-        return;
-    } else {
-        $saltVal = Utils::GenerateRandomString(150);
-        $hashedPass = hash('sha256', $saltVal . $password);
+        // Check if the user exists
+        if ($result->num_rows > 0) {
+            Response::CreateResponse(false, "Username already exists!");
+            return;
+        } else {
+            $saltVal = Utils::GenerateRandomString(150);
+            $hashedPass = hash('sha256', $saltVal . $password);
 
-        //insert acc into table
-        $stmt = $conn->prepare("INSERT INTO tbluserlogin VALUES (0, '" . $username . "', '" . $hashedPass . "', '" . $saltVal . "')");
+            //insert acc into table
+            $stmt = $conn->prepare("INSERT INTO tbluserlogin VALUES (0, '" . $username . "', '" . $hashedPass . "', '" . $saltVal . "')");
 
-        $result = $stmt->execute();
-        if ($result)
-            Response::CreateResponse(true, "Successfully created account!");
-        else
-            Response::CreateResponse(false, "Failed to Create Account!");
-    }
+            $result = $stmt->execute();
+            if ($result)
+                Response::CreateResponse(true, "Successfully created account!");
+            else
+                Response::CreateResponse(false, "Failed to Create Account!");
+        }
+    } else
+        Response::CreateResponse(false, "Invalid Request!");
+
 } finally {
     $conn->close();
 }
